2e189d9b6cbe54e083aaa0ccc77f405e592b2c85
Daniel Amesberger <daniel.amesberger@amescon.com>
2022-07-05 20:55:35 +0200

    we can now parse two styles of embedded Lua require'nvim-treesitter.configs'.setup {...} and require('vgit').setup({...})

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use analisar::aware::ast::{Args, FunctionCall, LiteralString, Name, Table, Field};
+use analisar::aware::ast::{Args, Field, FunctionCall, LiteralString, Name, Table};
 use analisar::aware::{
     ast::{Expression, Statement, Suffixed},
     Parser,
 };
 use color_eyre::Result;
 use regex::Regex;
+use std::cell::Cell;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
@@ -100,67 +101,143 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn parse_lua(lua_code: &str) {
+
+/// turns the first Arg into an owned String if possible.
+/// This only handles two cases:
+/// 1. the Arg is an Args::String(LiteralString))
+/// 2. the Arg is an ExpListItem
+///    - in that case the first argument if the expression list is returned as String
+fn args_to_lit_string(args: &Args) -> Option<String> {
+    match &args {
+        Args::String(LiteralString { span: _, value }) => Some(value.to_string()),
+        Args::ExpList {
+            open_paren: _,
+            exprs,
+            close_paren: _,
+        } if exprs.len() == 1 => {
+            if let analisar::aware::ast::ExpListItem::Expr(Expression::LiteralString(
+                LiteralString { span: _, value },
+            )) = &exprs[0]
+            {
+                Some(value.to_string())
+            } else {
+                None
+            }
+        }
+        _ => None,
+    }
+}
+
+fn args_to_table<'a> (args: &'a Args) -> Option<&'a Table<'a>> {
+    match &args {
+        Args::Table(t) => Some(t),
+        Args::ExpList {
+            open_paren: _,
+            exprs,
+            close_paren: _,
+        } if exprs.len() == 1 => {
+            if let analisar::aware::ast::ExpListItem::Expr(Expression::TableCtor(t)) = &exprs[0]
+            {
+                return Some(t);
+            } 
+            None
+        }
+        _ => None,
+    }
+}
+
+fn parse_lua(lua_code: &str) -> Option<Vec<String>> {
     if let Some(Ok(swc)) = Parser::new(lua_code.as_bytes()).next() {
         if let Statement::Expression(Expression::FuncCall(f)) = swc.statement {
-            if let Expression::Suffixed(s) = *f.prefix {
+            if let Expression::Suffixed(s) = &*f.prefix {
                 let Suffixed {
                     subject,
                     property: _,
-                } = *s;
-                //println!("subject {:#?}, property {:#?}", subject, property);
+                } = &**s;
                 if let Expression::FuncCall(FunctionCall { prefix, args }) = subject {
-                    if let (
+                    match &**prefix {
                         Expression::Name(Name {
-                            name_span,
+                            name_span: _,
                             name,
-                            attr,
-                        }),
-                        Args::String(LiteralString { span, value }),
-                    ) = (*prefix, args)
-                    {
-                        let name = name.to_string();
-                        let value = value[1..value.len() - 1].to_string();
-                        if let Args::Table(table) = f.args {
-                            match (name.as_str(), value.as_str()) {
-                                ("require", "nvim-treesitter.configs") => {
-                                    let mut path_str = String::new();
-                                    walk_fields(table.field_list, &mut path_str);
-                                }
-                                _ => {}
-                            };
-                        };
-                    }
-                }
+                            attr: _,
+                        }) => {
+                            if let Some(value) = args_to_lit_string(args) {
+                                return Some(create_map(name, &value, &f));
+                            }
+                        }
+                        _ => {
+                            eprintln!("ignored root: {:#?}", args);
+                        }
+                    };
+                };
             }
         }
     }
+    None
 }
 
-fn walk_fields(field_list: Vec<Field>, path_str: &mut String) {
+fn create_map(name: &str, value: &str, f: &FunctionCall) -> Vec<String> {
+    let mut mappings = vec![];
+
+    let name = name.to_string();
+    let value = value[1..value.len() - 1].to_string();
+    if let Some(table) = args_to_table(&f.args) {
+        if let ("require", root) = (name.as_str(), value.as_str()) {
+            let results = walk_fields(&table.field_list);
+            for result in results {
+                mappings.push(format!("{}.{}", root, result.into_inner()));
+            }
+
+            println!("{:#?}", &mappings);
+        };
+    };
+
+    mappings
+}
+
+fn walk_fields(field_list: &Vec<Field>) -> Vec<Cell<String>> {
+    let mut paths = vec![];
     for field in field_list {
         match field {
-            Field::Record { name, eq, value, sep } => {
-                //println!("Record: {:#?}", name);
-                expr(name, value, path_str);
-            },
-            Field::List { value, sep } => {
-                println!("list: {:#?}", value);
-            },
+            Field::Record {
+                name,
+                eq: _,
+                value,
+                sep: _,
+            } => {
+                //generate pathname
+                let pathname = match &name {
+                    Expression::Name(name) => Some(format!("{}.", name.name)),
+                    _ => None,
+                };
+
+                if let Some((lh, rh)) = match (name, value) {
+                    (Expression::LiteralString(lh), Expression::LiteralString(rh)) => {
+                        Some((lh.value.to_string(), rh.value.to_string()))
+                    }
+                    (_, Expression::TableCtor(t)) => {
+                        let child_paths = walk_fields(&t.field_list);
+                        if let Some(pathname) = &pathname {
+                            for mut child in child_paths {
+                                child.get_mut().insert_str(0, pathname.as_str());
+                                paths.push(child);
+                            }
+                        }
+                        None
+                    }
+                    (_lh, _rh) => {
+                        None
+                    }
+                } {
+                    let leaf = Cell::new(format!("{}:{}", rh, lh));
+                    paths.push(leaf);
+                }
+            }
+            Field::List { value: _, sep: _ } => {
+                //println!("list: {:#?}", value);
+            }
         }
     }
-}
 
-fn expr(lh: Expression, rh: Expression, path_str: &mut String) {
-    match (lh, rh) {
-        (Expression::LiteralString(lh), Expression::LiteralString(s)) => { 
-            println!("{}: {}", lh.value, s.value);
-        },
-        (lh, Expression::TableCtor(t)) => { 
-            //path_str.push_str(lh.value);
-            println!("iterate {:#?}", lh);
-            walk_fields(t.field_list, path_str); },
-        (_, _) => ()
-    }
-
+    paths
 }
